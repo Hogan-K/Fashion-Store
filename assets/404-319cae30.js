68ddae45a497651c42c915e36edebdbaaf6f1b35
HoganK <ooss87968863@gmail.com>
2023-10-11 11:22:21 +0800

    測試調整路徑3

--- assets/404-319cae30.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,m as n,n as c}from"./index-d27a65bb.js";const t={};function o(r,_){return n(),c("h1",null,"404 not defined.")}const a=e(t,[["render",o]]);export{a as default};
---
+++ assets/404-d8d007de.js
@@ -0,0 +1,1 @@
+import{_ as e,m as n,n as c}from"./index-23bea264.js";const t={};function o(r,_){return n(),c("h1",null,"404 not defined.")}const a=e(t,[["render",o]]);export{a as default};
